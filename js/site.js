51eb07099b63a80399edf5475ee62036624d2b29
mariamabarry <barry.hadja16@gmail.com>
2023-11-20 00:12:16 -0500

    App is functional
    Able to generate and display results on button click

--- js/site.js
+++ js/site.js
@@ -1,3 +1,49 @@
-function HelloWorld(){
-    alert("hello world!");
+/* Controller Function*/
+function main()
+{
+    //Get fizz and buzz values
+    fizz = parseInt(document.getElementById("fizzInput").value);
+    buzz = parseInt(document.getElementById("buzzInput").value);
+
+    //create list
+    let list = createList(fizz, buzz);
+
+    //display list
+    displayList(list);
+}
+function createList(fizz, buzz)
+{
+    let arrLen = 100;
+    const arr = [];
+    for(let i= 1; i<arrLen + 1; i++)
+    {
+        if(i % fizz == 0 && i % buzz != 0)
+        {
+            arr[i] ="Fizz";
+        }
+        else if(i % buzz == 0 && i % fizz != 0)
+        {
+            arr[i] = "Buzz";
+        }
+        else if (i % fizz == 0 && i % buzz == 0)
+        {
+            arr[i] = "FizzBuzz";
+        }
+        else
+        {
+            arr[i] = i;
+        }
+    }
+
+    return arr;
+}
+
+function displayList(list)
+{
+    let tableRows = "";
+    for(let i = 1; i < list.length; i++)
+    {
+        tableRows += `<tr><td>${list[i]}</td></tr>`;
+    }
+    document.getElementById("results").innerHTML = tableRows;  
 }
